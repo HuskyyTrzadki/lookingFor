16890067ec41390cd6ad778907ad02c9008d694f
HuskyyTrzaski <kubajochymek@gmail.com>
2021-08-11 13:54:00 +0200

    starting page layout done

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,12 +1,12 @@
 function Circle() {
   return (
     <div className="loginCircle">
-      <div id="fb" className="quarter"></div>
-      <div id="mail" className="quarter"></div>
-      <div id="decline" className="half">
+      <button id="fb" className="quarter"></button>
+      <button id="mail" className="quarter"></button>
+      <button id="decline" className="half">
         {" "}
-        no, maybe later
-      </div>
+        <p>no, maybe later</p>
+      </button>
     </div>
   );
 }
