059461a0bc7e45a23758ac6f17cc0960a0559d3e
HuskyyTrzaski <kubajochymek@gmail.com>
2021-08-14 19:12:58 +0200

    first test of  login form

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { useState } from "react";
 import LoginPage from "./components/LoginPage/LoginPage";
 const App = () => {
-  const adminUser = {
-    email: "admin@admin.com",
-    password: "admin123",
-  };
-  const [user, setUser] = useState({ name: "", email: "" });
+  const [user, setUser] = useState("");
+
   return (
     <div>
-      <LoginPage />
+      <p>hello {user}</p>
+      <LoginPage Login={setUser} />
     </div>
   );
 };
